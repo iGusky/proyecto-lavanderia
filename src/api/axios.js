0de8e7f33a80081708b2e11780ede6a2a6dbcfc9
AlfonsoJari <alfonso.jari@gmail.com>
2021-11-08 23:00:39 -0600

    Inicializacion ListaVenta

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const clienteAxios = axios.create({
-  baseURL: 'http://127.0.0.1:4000'
+  baseURL: 'https://lavanderia-backend.herokuapp.com'
 });
 
 export default clienteAxios;
--- src/components/ListaVentas/ListaVentasPage.tsx
+++ src/components/ListaVentas/ListaVentasPage.tsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
 
 const ListaVentasPage = () => {
-  return (
-    <div className="container">
-      <h1>Lista de ventas</h1>
-    </div>
-  )
-}
+  const [listaVentas, setList] = useState([] as any[]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await Axios({
+          url: "https://lavanderia-backend.herokuapp.com/ventas",
+        });
+        setList(response.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-export default ListaVentasPage
+    fetchData();
+  }, [setList]);
+
+  return <div className="ListaVentasPage">{}</div>;
+};
+
+export default ListaVentasPage;
