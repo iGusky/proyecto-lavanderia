bd2f335cc30868c7386047d63473936153077aab
Gustavo Hernandez <gustavohdezmorales@gmail.com>
2021-12-10 14:46:12 -0600

    build scripts added

---
+++ public/main.js
@@ -0,0 +1,44 @@
+const electron = require('electron');
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+require('@electron/remote/main').initialize()
+
+let mainWindow;
+
+function createWindow() {
+  const { width, height } = electron.screen.getPrimaryDisplay().workAreaSize
+  mainWindow = new BrowserWindow({
+    width, height, resizable: false, show: false,
+    webPreferences: {
+      nodeIntegration: true,
+      enableRemouteModule: true
+    }
+  });
+  mainWindow.maximize();
+  mainWindow.show();
+  mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
+  if (isDev) {
+    // Open the DevTools.
+    //BrowserWindow.addDevToolsExtension('<location to your react chrome extension>');
+    mainWindow.webContents.openDevTools();
+  }
+  mainWindow.on('closed', () => mainWindow = null);
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
